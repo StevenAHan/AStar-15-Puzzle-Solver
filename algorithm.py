2837b4f5346e8fbbf1cca151006bc8fe95039175
majikyouser <sahiagb@gmail.com>
2022-10-25 20:39:05 -0400

    Finished write_solution_to_file

--- algorithm.py
+++ algorithm.py
@@ -36,17 +36,22 @@ def read_file(file_name):
     pass
 
 def write_solution_to_file(original, goal, weight, depth, total_nodes, string_of_actions, A_costs_string):
-    write_to_file = ""
+    text_file = open("output.txt", "w")
     for row in original:
-        write_to_file += str(row) + "\n"
-    write_to_file += "\n"
+        for elem in row:
+            text_file.write(str(elem) + " ")
+        text_file.write("\n")
+    text_file.write("\n")
     for row in goal:
-        write_to_file += str(row) + "\n"
-    write_to_file += "\n" + weight + "\n" + depth + "\n" + total_nodes + "\n"
-    write_to_file += string_of_actions + "\n" + A_costs_string 
-    #Turn string into file
-    text_file = open("D:/data.txt", "w")
-    text_file.write(write_to_file)
+        for elem in row:
+            text_file.write(str(elem) + " ")
+        text_file.write("\n")
+    text_file.write("\n" + weight + "\n" + depth + "\n" + total_nodes + "\n")
+    for elem in string_of_actions:
+        text_file.write(str(elem) + " ")
+    text_file.write("\n")
+    for elem in A_costs_string:
+        text_file.write(str(elem) + " ")
     text_file.close()
     return
 
@@ -55,7 +60,7 @@ def main():
     list1.append([1,2,3])
     list1.append([4,5,6])
     list1.append([7,8,9])
-    write_solution_to_file(list1, list1, "c", "d", "e", [1,2,3], ["a","b","c","d"]) # Crying cause it don't work rn T_T
+    write_solution_to_file(list1, list1, "c", "d", "e", [1,2,3], ["a","b","c","d"]) # IT WORKS
 
 if __name__ == "__main__":
     main()
